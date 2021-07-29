f95408d793569e7f4a754feeec2059ae08594b4f
mogou24 <86212920+mogou24@users.noreply.github.com>
2021-07-29 16:27:20 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,31 @@
+//滑動
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//LIGHTBOX
+lightbox.option({
+resizeDuration: 200,
+wrapAround: true,
+});
