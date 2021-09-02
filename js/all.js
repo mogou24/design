5d67e38819c31004b73757eb7a6b2519e079a1c5
mogou24 <86212920+mogou24@users.noreply.github.com>
2021-09-02 16:56:19 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,42 @@
+//滑動
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 8000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//LIGHTBOX
+lightbox.option({
+resizeDuration: 200,
+wrapAround: true,
+});
+
+//skrollr
+var s = skrollr.init();// 初始化
